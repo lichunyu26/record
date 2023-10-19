8a0f9020436aef082ac546aac654b20cb240c311
lichunyu <lichunyu@slife.com>
2023-10-19 14:11:06 +0800

    first submit

--- public/afterInjectScript.js
+++
@@ -1,1 +0,0 @@
-console.log("afterInjectScript注入成功，注入位置为#app下")
--- vite-plugins/vue-hook-auto-import.ts
+++
@@ -1,7 +0,0 @@
-import AutoImport from "unplugin-auto-import/vite";
-
-export default  AutoImport({
-    imports: ['vue', 'vue-router', 'vuex', '@vueuse/head',"pinia"],
-    // 可以选择auto-import.d.ts生成的位置，使用ts建议设置为'src/auto-import.d.ts'
-    dts: './auto-import.d.ts'
-})
